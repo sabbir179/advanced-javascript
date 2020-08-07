088c7c0d31d6451b2a578cb102f9f57cf7d7bcc1
Sabbir Ahmed <sabbirahmed@Sabbirs-MacBook-Pro.local>
2020-08-07 21:15:46 +0600

    array, splice, sclice, scope, break, continue;

---
+++ array-splice.js
@@ -0,0 +1,8 @@
+const nums = [2, 3, 5, 7, 4, 8, 98];
+const part = nums.slice(2,5);
+// const removed = nums.splice(2, 5);
+// console.log(nums);
+// // console.log(part);
+// console.log(removed);
+const together = nums.join("-");
+console.log(together);
---
+++ scope.js
@@ -0,0 +1,10 @@
+let bouns = 20;
+
+function sum(first, second) {
+    let result = first + second + bouns;
+    return result;
+}
+
+const output = sum(3, 6);
+console.log(bouns);
+console.log(output);
